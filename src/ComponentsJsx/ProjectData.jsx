297b2f82bcedc024f8434a2f1f10bfe2e205a9d2
chiagozie pio <nduagubapio@gmail.com>
2024-08-17 15:49:19 +0100

    some changes to new projects's link

--- src/ComponentsJsx/ProjectData.jsx
+++ src/ComponentsJsx/ProjectData.jsx
@@ -1,15 +1,15 @@
  export const datas = [
     {
-        id:2,
+        id:1,
         img: "https://1drv.ms/i/c/d32039eb0b48ba39/IQN5ROLCeBneR69FizYycOyqAXh56LGmmA51dFzyYiKg6vo?width=1024",
         name: "Cozy-home",
-        des:"An Apartment rental site for vacations and relaxings. As a user, you can book an apartment, check geo-location of the apartment and also you can list an apartment for renting. But you to be a registered user. The github link to main github account where you can search for both the frontend and backend source code",
+        des:"An Apartment rental site for vacations and relaxings. As a user, you can book an apartment, check geo-location of the apartment and also you can list an apartment for renting. But you to be a registered user. The github link leads to the frontend source code, go back to Respositories tab to see the backend code named as airbnb-backend",
         livePage: "https://cozy-home-three.vercel.app/",
-        githubLink:"https://github.com/chiagoziepio",
+        githubLink:"https://github.com/chiagoziepio/airbnb-frontend",
         instruction: false
     },
     {
-        id:1,
+        id:2,
         img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21346&authkey=%21AJU3DieSBPZKiiw&width=1366&height=690",
         name: "Shoe castle",
         des:"An E-commerce website built with React js, CSS with libraries like react-icon, react-router-dom.",
@@ -18,7 +18,7 @@
         instruction: true
     },
     {
-        id:2,
+        id:3,
         img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21347&authkey=%21ALzRXYVw6Yel_ig&width=1366&height=698",
         name: "Trendings.co",
         des:"A blog website built with React js,CSS with libraries that include date-fns, react-router-dom, react-icon",
@@ -27,7 +27,7 @@
         instruction: false
     },
     {
-        id:3,
+        id:4,
         img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21345&authkey=%21ANe3YqcbQjYy77g&width=1366&height=686",
         name: "Todo web App",
         des:"A Todo web App built with React js, with the inclusion of date-fns library",
