3d5007b075971b92611db294d9962eedd46d51c0
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-11-04 10:51:40 +0100

    updated my stacks

--- src/ComponentsJsx/Hero.jsx
+++ src/ComponentsJsx/Hero.jsx
@@ -1,49 +1,87 @@
-import React from 'react'
-import '../ComponentsCss/Hero.css'
+import "../ComponentsCss/Hero.css";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { MdAttachEmail } from "react-icons/md";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
-const Hero = ({download}) => {
-    
+const Hero = ({ download }) => {
   return (
-    <section className='hero' id='home'>
-        <div className='heroUpperPart'>
-            <h2>Hi I am <span className='name'>CHRISTOPHER</span><br /> A deligient and dedicated <span className='category'>Frontend developer</span></h2>
-            <button onClick={download} className='cv' ><FaCloudDownloadAlt size={20}className='download_icon'/> Resume</button>
+    <section className="hero" id="home">
+      <div className="heroUpperPart">
+        <h2>
+          Hi I am <span className="name">CHRISTOPHER</span>
+          <br /> A deligient and dedicated{" "}
+          <span className="category">Frontend developer</span>
+        </h2>
+        <button onClick={download} className="cv">
+          <FaCloudDownloadAlt size={20} className="download_icon" /> Resume
+        </button>
+      </div>
+      <div className="hero_img">
+        <div className="dev_icons firstpart">
+          <img
+            src="https://i.pinimg.com/236x/5b/fa/e3/5bfae33bcc5e5efda6aaef3a8c83a174.jpg"
+            alt=""
+            className="reactpics icons"
+          />
+          <img
+            src="https://i.pinimg.com/236x/17/65/0d/17650db87fb5752364a9a4b6885290ac.jpg"
+            className="icons"
+            alt="css"
+          />
+          <img
+            src="https://i.pinimg.com/564x/f4/46/01/f44601df7b163687fc463dbceedbd4f7.jpg"
+            className="icons"
+            alt="tw"
+          />
         </div>
-        <div className="hero_img">
-            <div className="dev_icons firstpart">
-                <img src="https://i.pinimg.com/236x/5b/fa/e3/5bfae33bcc5e5efda6aaef3a8c83a174.jpg" alt="" className="reactpics icons" />
-                <img src="https://i.pinimg.com/236x/17/65/0d/17650db87fb5752364a9a4b6885290ac.jpg" className='icons' alt="css" />
-            </div>
-            <div className="main-container">
-            <motion.img src="https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21348&authkey=%21ANV0iQ2leK_3lfo&width=2320&height=3088" className='mainPics' alt="u"
+        <div className="main-container">
+          <motion.img
+            src="https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21348&authkey=%21ANV0iQ2leK_3lfo&width=2320&height=3088"
+            className="mainPics"
+            alt="u"
             initial={{
-                display:"none",
-                y:100
+              display: "none",
+              y: 100,
             }}
             animate={{
-                display:"block",
-                y:0
+              display: "block",
+              y: 0,
             }}
-            transition={{duration:2 , delay:1}}
-            />
-            </div>
-            <div className="dev_icons secondpart">
-                <img src="https://i.pinimg.com/236x/1e/e5/c0/1ee5c08f80a1c61d66a37fa64c2d984c.jpg"className='icons'  alt="js" />
-                <img src="https://i.pinimg.com/236x/b1/75/54/b175549d8268dd656b92e3b56988bdf9.jpg"className='icons'  alt="ht" />
-                <img src="https://i.pinimg.com/236x/29/37/2a/29372aa493607af0848468d30223ccc6.jpg" className='icons' alt="bs" />
-            </div>
+            transition={{ duration: 2, delay: 1 }}
+          />
         </div>
-        <div className="HeroLowerpart">
-            <h4>Ever since I started my journey in the tech world, i have developed the skill of Problem solving <br />I am also a lover of Knowledge and learning</h4>
-
-        <a href="mailto:christopherpaddy2017@gmail.com" className='mail'><MdAttachEmail className='mail_icon' size={20}/>mail me</a>
-
+        <div className="dev_icons secondpart">
+          <img
+            src="https://i.pinimg.com/236x/1e/e5/c0/1ee5c08f80a1c61d66a37fa64c2d984c.jpg"
+            className="icons"
+            alt="js"
+          />
+          <img
+            src="https://i.pinimg.com/236x/b1/75/54/b175549d8268dd656b92e3b56988bdf9.jpg"
+            className="icons"
+            alt="ht"
+          />
+          <img
+            src="https://i.pinimg.com/236x/29/37/2a/29372aa493607af0848468d30223ccc6.jpg"
+            className="icons"
+            alt="bs"
+          />
         </div>
+      </div>
+      <div className="HeroLowerpart">
+        <h4>
+          Ever since I started my journey in the tech world, i have developed
+          the skill of Problem solving <br />I am also a lover of Knowledge and
+          learning
+        </h4>
+
+        <a href="mailto:christopherpaddy2017@gmail.com" className="mail">
+          <MdAttachEmail className="mail_icon" size={20} />
+          mail me
+        </a>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/ComponentsJsx/Stack.jsx
+++ src/ComponentsJsx/Stack.jsx
@@ -1,110 +1,173 @@
-import React, { useRef } from 'react'
-import '../ComponentsCss/Stack.css'
-import { motion, useInView } from 'framer-motion'
+import { useRef } from "react";
+import "../ComponentsCss/Stack.css";
+import { motion, useInView } from "framer-motion";
 
 const Stack = () => {
-    const ref = useRef()
- useInView(ref, { once:true })
- 
+  const ref = useRef();
+  useInView(ref, { once: true });
+
   return (
-    <section className='stack' id="stack">
-        <motion.h2  ref={ref} className="sectionTitle"
+    <section className="stack" id="stack">
+      <motion.h2
+        ref={ref}
+        className="sectionTitle"
         initial={{
-            x:-300
+          x: -300,
         }}
         whileInView={{
-            x:0
+          x: 0,
         }}
         transition={{
-            duration:2,
-
+          duration: 2,
         }}
-        ><span className="firstLetter">M</span>y <span className="secLetter">S</span>tack</motion.h2>
-        <div className="stackContainer">
-            <div className='stackBx'>
-                <img src="https://i.pinimg.com/236x/5b/fa/e3/5bfae33bcc5e5efda6aaef3a8c83a174.jpg" alt="" />
-                <div className="progressBarBx">
-                    <motion.div className="progressBar reactbar"
-                        initial={{
-                            width:0
-                        }}
-                        whileInView={{
-                            width:"70%"
-                        }}
-                        transition={{
-                            duration:2
-                        }}
-                    >70%</motion.div>
-                </div>
-            </div>
-            <div className='stackBx'>
-                <img src="https://i.pinimg.com/236x/1e/e5/c0/1ee5c08f80a1c61d66a37fa64c2d984c.jpg" alt="" />
-                <div className="progressBarBx">
-                    <motion.div className="progressBar jsbar"
-                        initial={{
-                            width:0
-                        }}
-                        whileInView={{
-                            width:"50%"
-                        }}
-                        transition={{
-                            duration:2
-                        }}
-                    >50%</motion.div>
-                </div>
-            </div>
-            <div className='stackBx'>
-                <img src="https://i.pinimg.com/236x/17/65/0d/17650db87fb5752364a9a4b6885290ac.jpg" alt="" />
-                <div className="progressBarBx">
-                    <motion.div className="progressBar cssbar"
-                        initial={{
-                            width:0
-                        }}
-                        whileInView={{
-                            width:"60%"
-                        }}
-                        transition={{
-                            duration:2
-                        }}
-                    >60%</motion.div>
-                </div>
-            </div>
-            <div className='stackBx'>
-                <img src="https://i.pinimg.com/236x/b1/75/54/b175549d8268dd656b92e3b56988bdf9.jpg" alt="" />
-                <div className="progressBarBx">
-                    <motion.div className="progressBar htmlbar"
-                        initial={{
-                            width:0
-                        }}
-                        whileInView={{
-                            width:"80%"
-                        }}
-                        transition={{
-                            duration:2
-                        }}
-                    >80%</motion.div>
-                </div>
-            </div>
-            <div className='stackBx'>
-                <img src="https://i.pinimg.com/236x/29/37/2a/29372aa493607af0848468d30223ccc6.jpg" alt="" />
-                <div className="progressBarBx">
-                    <motion.div className="progressBar bsbar"
-                        initial={{
-                            width:0
-                        }}
-                        whileInView={{
-                            width:"90%"
-                        }}
-                        transition={{
-                            duration:2
-                        }}
-                    >90%</motion.div>
-                </div>
-            </div>
+      >
+        <span className="firstLetter">M</span>y{" "}
+        <span className="secLetter">S</span>tack
+      </motion.h2>
+      <div className="stackContainer">
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/236x/5b/fa/e3/5bfae33bcc5e5efda6aaef3a8c83a174.jpg"
+            alt=""
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar reactbar"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "70%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+            >
+              70%
+            </motion.div>
+          </div>
         </div>
-    
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/236x/1e/e5/c0/1ee5c08f80a1c61d66a37fa64c2d984c.jpg"
+            alt=""
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar jsbar"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "50%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+            >
+              50%
+            </motion.div>
+          </div>
+        </div>
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/236x/17/65/0d/17650db87fb5752364a9a4b6885290ac.jpg"
+            alt=""
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar cssbar"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "60%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+            >
+              60%
+            </motion.div>
+          </div>
+        </div>
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/236x/b1/75/54/b175549d8268dd656b92e3b56988bdf9.jpg"
+            alt=""
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar htmlbar"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "80%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+            >
+              80%
+            </motion.div>
+          </div>
+        </div>
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/236x/29/37/2a/29372aa493607af0848468d30223ccc6.jpg"
+            alt=""
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar bsbar"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "90%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+            >
+              90%
+            </motion.div>
+          </div>
+        </div>
+        <div className="stackBx">
+          <img
+            src="https://i.pinimg.com/564x/f4/46/01/f44601df7b163687fc463dbceedbd4f7.jpg"
+            alt="tw"
+          />
+          <div className="progressBarBx">
+            <motion.div
+              className="progressBar tw"
+              initial={{
+                width: 0,
+              }}
+              whileInView={{
+                width: "90%",
+              }}
+              transition={{
+                duration: 2,
+              }}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                width: "90%",
+                height: "20px",
+                borderRadius: "16px",
+                background: "#020636",
+              }}
+            >
+              90%
+            </motion.div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Stack
+export default Stack;
