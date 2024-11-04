a2f48abdc3f686478be34c4b3396cc909d0f5042
christopherpio <nduaguba.christopher@standardlife.org.ng>
2024-11-04 10:37:24 +0100

    modified the cv and added new projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,55 @@
-import { useEffect, useState } from 'react'
-import Header from './ComponentsJsx/Header'
-import Hero from './ComponentsJsx/Hero'
-import Stack from './ComponentsJsx/Stack'
-import Projects from './ComponentsJsx/Projects'
-import {datas} from './ComponentsJsx/ProjectData'
-import Contact from './ComponentsJsx/Contact'
-import Footer from './ComponentsJsx/Footer'
-import MobileNav from './ComponentsJsx/MobileNav'
+import { useEffect, useState } from "react";
+import Header from "./ComponentsJsx/Header";
+import Hero from "./ComponentsJsx/Hero";
+import Stack from "./ComponentsJsx/Stack";
+import Projects from "./ComponentsJsx/Projects";
+import { datas } from "./ComponentsJsx/ProjectData";
+import Contact from "./ComponentsJsx/Contact";
+import Footer from "./ComponentsJsx/Footer";
+import MobileNav from "./ComponentsJsx/MobileNav";
 
 function App() {
   const [data, setData] = useState([]);
   const [isOpen, setIsopen] = useState(false);
-  const thirdUrl = "https://drive.google.com/file/d/1Al4_eiuiPgtEJuR-bEe-DQG5xtcCSZbj/view?usp=sharing";
-  const linkUrl = "https://drive.google.com/uc?export=download&id=1Al4_eiuiPgtEJuR-bEe-DQG5xtcCSZbj"
-  const Handle_menuShow= ()=>{
-    
-    setIsopen(!isOpen)
-    if(!isOpen){
-      document.body.classList.add("stop")
-    }else{
-      document.body.classList.remove("stop")
+  const linkUrl =
+    "https://drive.google.com/uc?export=download&id=1Al4_eiuiPgtEJuR-bEe-DQG5xtcCSZbj";
+  const url =
+    "https://docs.google.com/document/d/1PvKiRNNIR3OfqJxPgocpc5vK3emrrCF6gyeQNmybIKI/export?format=pdf";
+  const Handle_menuShow = () => {
+    setIsopen(!isOpen);
+    if (!isOpen) {
+      document.body.classList.add("stop");
+    } else {
+      document.body.classList.remove("stop");
     }
-  }
-  const download =  ()=>{
-    const a = document.createElement('a');
-    document.body.appendChild(a)
-    a.href = linkUrl
-    a.download = "chiagozie_cv.pdf"
-    a.click()
-    document.body.removeChild(a)    
-    /* fetch('http://localhost:3000/proxy').then(resp => resp.blob()).then(blob => {
-        
-        let alink = document.createElement("a");
-        let fileURL = URL.createObjectURL(blob)
-        alink.style.display= "none";
-        alink.href = fileURL;
-        alink.download = "nduaguba_chiagozie_dev_cv.pdf";
-        document.body.appendChild(alink);
-        alink.click();
-        document.body.removeChild(alink);
-        window.URL.revokeObjectURL(fileURL);
-    })  */
-    
-}
-  
-  useEffect(()=>{
-      setData(datas)
-  },[])
-  return(
-    <div className='app'>
-        <Header Handle_menuShow={Handle_menuShow}/>
-        <Hero download={download}/>
-        <Stack/>
-        <Projects data={data}/>
-        <Contact download={download}/>
-        <MobileNav isOpen={isOpen} download={download} Handle_menuShow={Handle_menuShow}/>
-        <Footer/>
+  };
+  const download = () => {
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = "nduaguba_chiagozie_cv.pdf";
+    a.click();
+    document.body.removeChild(a);
+  };
+
+  useEffect(() => {
+    setData(datas);
+  }, []);
+  return (
+    <div className="app">
+      <Header Handle_menuShow={Handle_menuShow} />
+      <Hero download={download} />
+      <Stack />
+      <Projects data={data} />
+      <Contact download={download} />
+      <MobileNav
+        isOpen={isOpen}
+        download={download}
+        Handle_menuShow={Handle_menuShow}
+      />
+      <Footer />
     </div>
-  )
-  
+  );
 }
 
-export default App
+export default App;
--- src/ComponentsJsx/ProjectData.jsx
+++ src/ComponentsJsx/ProjectData.jsx
@@ -1,38 +1,39 @@
- export const datas = [
-    {
-        id:1,
-        img: "https://1drv.ms/i/c/d32039eb0b48ba39/IQN5ROLCeBneR69FizYycOyqAXh56LGmmA51dFzyYiKg6vo?width=1024",
-        name: "Cozy-home",
-        des:"An Apartment rental site for vacations and relaxings. As a user, you can book an apartment, check geo-location of the apartment and also you can list an apartment for renting. But you to be a registered user. The github link leads to the frontend source code, go back to Respositories tab to see the backend code named as airbnb-backend",
-        livePage: "https://cozy-home-three.vercel.app/",
-        githubLink:"https://github.com/chiagoziepio/airbnb-frontend",
-        instruction: false
-    },
-    {
-        id:2,
-        img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21346&authkey=%21AJU3DieSBPZKiiw&width=1366&height=690",
-        name: "Shoe castle",
-        des:"An E-commerce website built with React js, CSS with libraries like react-icon, react-router-dom.",
-        livePage: "https://shoe-castle-alt-web.vercel.app/",
-        githubLink:"https://github.com/chiagoziepio/shoeCastle-alt.git",
-        instruction: true
-    },
-    {
-        id:3,
-        img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21347&authkey=%21ALzRXYVw6Yel_ig&width=1366&height=698",
-        name: "Trendings.co",
-        des:"A blog website built with React js,CSS with libraries that include date-fns, react-router-dom, react-icon",
-        livePage: "https://trendings-hazel.vercel.app/",
-        githubLink:"https://github.com/chiagoziepio/trendings.git",
-        instruction: false
-    },
-    {
-        id:4,
-        img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21345&authkey=%21ANe3YqcbQjYy77g&width=1366&height=686",
-        name: "Todo web App",
-        des:"A Todo web App built with React js, with the inclusion of date-fns library",
-        livePage: "https://todo-list-ashen-eta.vercel.app/",
-        githubLink:"https://github.com/chiagoziepio/todo-list.git",
-        instruction: false
-    }
-]
+export const datas = [
+  {
+    id: 1,
+    img: "https://1drv.ms/i/c/d32039eb0b48ba39/IQN5ROLCeBneR69FizYycOyqAXh56LGmmA51dFzyYiKg6vo?width=1024",
+    name: "Cozy-home",
+    des: "An Apartment rental site for vacations and relaxing. As a user, you can book an apartment, check geo-location of the apartment and also you can list an apartment for renting. But you to be a registered user.",
+    livePage: "https://cozy-home-three.vercel.app/",
+    githubLink: "https://github.com/chiagoziepio/airbnb-frontend",
+    instruction: false,
+  },
+
+  {
+    id: 2,
+    img: "https://1drv.ms/i/c/d32039eb0b48ba39/IQSnL2qIEM5YTpJqoxwN0rxeARblk4L8dv03gfnAZcfVQjM?width=1024",
+    name: "CarePoint",
+    des: "A hospital management system built with React js,CSS with libraries that include antd, react-router-dom, react-icon, redux-toolKit, express, and mongoose",
+    livePage: "https://care-point-two.vercel.app/",
+    githubLink: "https://github.com/chiagoziepio/carePoint",
+    instruction: false,
+  },
+  {
+    id: 3,
+    img: "https://1drv.ms/i/c/d32039eb0b48ba39/IQRaCeYw0LciSKbUeBJFHdJKAQhuBpt-_AcbPsjOouemIbA?width=1024",
+    name: "Meetify",
+    des: "A social web app for saving memories and chatting built with React js,CSS with libraries that include react-router-dom, react-icon, redux-toolKit, express, and mongoose",
+    livePage: "https://meetify-pearl.vercel.app/",
+    githubLink: "https://github.com/chiagoziepio/meetify-frontend",
+    instruction: false,
+  },
+  {
+    id: 4,
+    img: "https://onedrive.live.com/embed?resid=D32039EB0B48BA39%21347&authkey=%21ALzRXYVw6Yel_ig&width=1366&height=698",
+    name: "Trendings.co",
+    des: "A blog website built with React js,CSS with libraries that include date-fns, react-router-dom, react-icon",
+    livePage: "https://trendings-hazel.vercel.app/",
+    githubLink: "https://github.com/chiagoziepio/trendings.git",
+    instruction: false,
+  },
+];
